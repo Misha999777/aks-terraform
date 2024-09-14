c7d4ddd6e93e903e608ed54f0739588ab16e7455
Михайло Грошевий <misha999777@gmail.com>
2024-09-14 23:36:19 +0200

    Try to use Azure AKS web app routing

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -91,5 +91,5 @@ resource "azurerm_role_assignment" "storage_blob_contributor" {
 resource "azurerm_role_assignment" "dns_zone_contributor" {
   principal_id         = azurerm_user_assigned_identity.workload-identity.principal_id
   role_definition_name = "DNS Zone Contributor"
-  scope                = azurerm_kubernetes_cluster.k8s.web_app_routing[0].web_app_routing_identity[0].client_id
+  scope                = azurerm_kubernetes_cluster.k8s.web_app_routing[0].web_app_routing_identity[0].object_id
 }
